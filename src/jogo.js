5128ed7f219723adc1649a64d8139699d4c08270
Philippe <philippe.idalgoprestes@gmail.com>
2023-05-10 17:49:48 -0300

    Todos os requisitos

--- src/jogo.js
+++ src/jogo.js
@@ -1,14 +1,41 @@
+import palavras from "./palavras"
+
 export default function Jogo(){
+   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
    var palavra_letras = [];
-    return(
+   var palavra;
+   function escolher_palavra(){
+    document.getElementById('palavra').style.color = 'rgb(0, 0, 0)';
+    document.getElementById('jogo_img').src = "./assets/forca0.png";
+    localStorage.setItem('foundtotal', 0);
+    let conj = palavras;
+    function comparador() { 
+        return Math.random() - 0.5;
+    }
+    conj.sort(comparador);
+    palavra = conj[0];
+    localStorage.setItem('palavra-driven', palavra);
+    localStorage.setItem('erros-driven', 0);
+    document.getElementById('palavra').innerHTML = "";
+    for (let a = 0; a < palavra.length; a++){
+        palavra_letras.push(palavra[a]);
+        let div = document.createElement('div');
+        div.classList.add('letra');
+        div.innerHTML = '_';
+        div.id = "letra" + a;
+        document.getElementById('palavra').appendChild(div);
+    }
+    for (let b = 0; b < alfabeto.length; b++){
+        document.getElementById(alfabeto[b]).setAttribute('class', 'letra-button-abled')
+        document.getElementById(alfabeto[b]).disabled = false;
+    }
+   }
+   return(
         <div class="jogo-div">
-            <img id= "jogo_img" class="jogo-img" src="./assets/forca0.png"></img>
+            <img id= "jogo_img" class="jogo-img" src="./assets/forca0.png" data-test="game-image"></img>
             <div class="jogo-letras-botao-div">
-                <button class="jogo-button">Escolher palavra</button>
-                <div id="palavra" class="palavra">
-                    {palavra_letras.map((letter) =>
-                    <div class='letra'>_</div>
-                    )}
+                <button class="jogo-button" onClick={escolher_palavra} data-test="choose-word">Escolher palavra</button>
+                <div id="palavra" class="palavra" data-test="word">
                 </div>
             </div>
         </div>
--- src/letras.js
+++ src/letras.js
@@ -1,9 +1,48 @@
+import React from 'react'
+
 export default function Letras(){
-    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    localStorage.setItem('foundtotal', 0);
+    let foundtotal = localStorage.getItem('foundtotal');
+    function clique_letra(e){
+        let id = e.target.id;
+        document.getElementById(id).setAttribute('class', 'letra-button-disabled');
+        document.getElementById(id).disabled = true;
+        let palavra = localStorage.getItem('palavra-driven');
+        let found = 0;
+        for (let a = 0; a < palavra.length; a++){
+            if (id == palavra[a]){
+                document.getElementById('letra' + a).innerHTML = id.toUpperCase();
+                found = 1;
+                foundtotal = localStorage.getItem('foundtotal');
+                localStorage.setItem('foundtotal', foundtotal + 1);
+            }
+        }
+        if (found == 0){
+            let erros = Number(localStorage.getItem('erros-driven')) + 1;
+            localStorage.setItem('erros-driven', erros);
+            document.getElementById('jogo_img').src = "./assets/forca" + erros + '.png';
+            if (erros == 6 || foundtotal == palavra.length){
+                for (let b = 0; b < alfabeto.length; b++){
+                    document.getElementById(alfabeto[b]).setAttribute('class', 'letra-button-disabled');
+                    document.getElementById(alfabeto[b]).disabled = true;
+                }
+                for (let a = 0; a < palavra.length; a++){
+                    document.getElementById('letra' + a).innerHTML = palavra[a].toUpperCase();
+                }
+                if (erros == 6){
+                    document.getElementById('palavra').style.color = 'rgb(255, 0, 0)';
+                }
+                else if (foundtotal == palavra.length){
+                    document.getElementById('palavra').style.color = 'rgb(0, 255, 0)';
+                }
+            }
+        }
+    }
     return(
         <div class='letras'>
             {alfabeto.map((letra) =>
-            <button class="letra-button-disabled" disabled="disabled">{letra.toUpperCase()}</button>
+            <button class="letra-button-disabled" onClick={(e) => clique_letra(e)} id={letra} data-test="letter">{letra.toUpperCase()}</button>
             )}
         </div>
     )
